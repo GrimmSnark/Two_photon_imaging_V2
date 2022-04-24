a12351bbfc1fb5b7022dc6001fba81121c2b40ea
Michael Savage <msavage@uabmc.edu>
2022-04-24 10:31:59 +0100

    Update subplotEvenAxes.m

--- plotting/utilities/subplotEvenAxes.m
+++ plotting/utilities/subplotEvenAxes.m
@@ -1,31 +1,45 @@
-function subplotEvenAxes(axH)
-% Sets both X andf Y axes to be the same across an entire subplot image
+function subplotEvenAxes(figH, axis2Use)
+% Sets both X,Y,Z axes to be the same across an entire subplot image
+%
+% Inputs: figH - figure handle to even out
+%         axis2Use - vector to specify axes to even out (x y z)
+%                    DEFAULT = all axis [1 1 1]
+
+%% defaults
+if nargin < 2 || isempty(axis2Use)
+    axis2Use = [1 1 1];
+end
 
 %% check if the handle is a figure
-if ishandle(axH) && findobj(axH,'type','figure')==axH
-    axH = findall(axH,'type','axes');
+if ishandle(figH) && findobj(figH,'type','figure')==figH
+    axH = findall(figH,'type','axes');
+end
+
+%% X axis
+if axis2Use(1) == 1
+    axXLim =  get(axH,'XLim');
+    maxX = max([axXLim{:}]);
+    minX = min([axXLim{:}]);
+    set(axH,'XLim',[minX maxX]);
 end
 
 %% Y axis
-axYLim =  get(axH,'YLim');
-maxY = max([axYLim{:}]);
-minY = min([axYLim{:}]);
-set(axH,'YLim',[minY maxY]);
-
-%% X axis
-axXLim =  get(axH,'XLim');
-maxX = max([axXLim{:}]);
-minX = min([axXLim{:}]);
-set(axH,'XLim',[minX maxX]);
-
-%% Z axis
-try
-axZLim =  get(axH,'ZLim');
-maxZ = max([axZLim{:}]);
-minZ = min([axZLim{:}]);
-set(axH,'ZLim',[minZ maxZ]);
-catch
-
+if axis2Use(2) == 1
+    axYLim =  get(axH,'YLim');
+    maxY = max([axYLim{:}]);
+    minY = min([axYLim{:}]);
+    set(axH,'YLim',[minY maxY]);
 end
 
+%% Z axis
+if axis2Use(3) == 1
+    try
+        axZLim =  get(axH,'ZLim');
+        maxZ = max([axZLim{:}]);
+        minZ = min([axZLim{:}]);
+        set(axH,'ZLim',[minZ maxZ]);
+    catch
+        
+    end
+end
 end
